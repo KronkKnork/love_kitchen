fb1a4f781d0c990322767481aa2828194467516a
KronkKnork <StrongKronk@yandex.ru>
2020-05-22 20:22:51 +0500

    popup main

--- src/js/main-prompt.js
+++ src/js/main-prompt.js
@@ -9,42 +9,24 @@ $(document).ready(function(){
             left_w = cp.left - 150,
             top_w = top - 60;
         if (left + prompt.width() > $(document).width()) {
+            prompt.text($(this).data("prompt"));
             prompt.show().offset({top: top_w, left: left_w});
         } else {
+            prompt.text($(this).data("prompt"));
             prompt.show().offset({top: top, left: left});
         }
     }).mouseout(function(){
         prompt.hide();
     });
 
+    
+    $('.main-btn').on('click', function(){
+        console.log($('#popup-free-design'));
+        $('#popup-free-design').show("slow");
+    });
+
+    $('.popup-close').on('click', function(){
+        $(this).parents('.popup').first().hide("slow");
+    });
 
 })
-
-/*$(document).ready(function(){
-    $(".circle-plus").hover(function() {
-        $(this).stop().animate({ backgroundColor: "#FDB73B"}, 400);
-    },function() {
-        $(this).stop().animate({ backgroundColor: "#ffffff" }, 400);
-    });
-});
-
-
-$(document).ready(function(){
-    console.log("sdjflsjk");
-    $('.circle-plus').on('mouseover', function(){
-        $(this).css('background', '#FDB73B')
-        console.log($(this));
-    })
-});*/
-
-
-
-// console.log("kfsdalkfgkldsafd");
-// $('.circle-plus').hover(function(){ // задаем функцию при наведении курсора на элемент	
-// 	 $( '.circle-plus' ).css( 'background', '#FDB73B' ) // задаем цвет заднего фона
-// 	  	   .text("Курсор на элементе"); // задаем текстовое содержимое
-// 	}, function(){ // задаем функцию, которая срабатывает, когда указатель выходит из элемента 	
-// 	$( "div" ).css( "background", "red" ) // задаем цвет заднего фона
-// 	  	      .text("Курсор вышел из элемента"); // задаем текстовое содержимое
-// });
-
